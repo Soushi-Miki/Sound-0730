b77cd34eb80bc4cf37ba04cf6e3fff193e57ccd5
Soushi-Miki <s2081145@s.konan-u.ac.jp>
2021-07-30 23:39:03 +0900

    Sound 0730 2

--- app/src/main/java/app/miki/mikiso/sound0726/MainActivity.kt
+++ app/src/main/java/app/miki/mikiso/sound0726/MainActivity.kt
@@ -3,7 +3,10 @@ package app.miki.mikiso.sound0726
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.MotionEvent.ACTION_DOWN
 import android.widget.ImageView
+import androidx.constraintlayout.widget.ConstraintSet
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,13 +19,64 @@ class MainActivity : AppCompatActivity() {
         //ドラムのサウンドファイルを読み込んで、プレイヤーを作る
         val drumSound : MediaPlayer = MediaPlayer.create(this,R.raw.seikai)
 
-        //ドラムがタップされたときに呼ばれる
-        drumImage.setOnClickListener {
-            //ドラムの音を巻き戻す
-            drumSound.seekTo(0)
+        //ドラムがタッチされたときに呼ばれる
+        drumImage.setOnTouchListener{ view, event ->
 
-            //ドラムの音を再生する
-            drumSound.start()
+            //タッチが始まった時に
+            if (event.action == MotionEvent.ACTION_DOWN){
+
+                //ドラムが鳴っている画像に切り替える
+                drumImage.setImageResource(R.drawable.kyouryuu)
+
+                //ドラムの音を巻き戻す
+                drumSound.seekTo(0)
+
+                //ドラムの音を再生する
+                drumSound.start()
+            }
+            //タッチが終るときに
+            else if (event.action == MotionEvent.ACTION_UP){
+                //ドラムが鳴っていない画像に切り替える
+                drumImage.setImageResource(R.drawable.kodaiko)
+            }
+
+            true
+
         }
+
+        val  pianoImage : ImageView = findViewById(R.id.piano_image)
+        val pianoSound : MediaPlayer = MediaPlayer.create(this,R.raw.kikon)
+
+        pianoImage.setOnTouchListener {view, event ->
+
+          if ( event.action == MotionEvent.ACTION_DOWN)  {
+              pianoImage.setImageResource(R.drawable.h)
+              pianoSound.seekTo(0)
+              pianoSound.start()
+          }
+            else if (event.action == MotionEvent.ACTION_UP){
+                pianoImage.setImageResource(R.drawable.buta)
+            }
+            true
+
+        }
+
+        val guitarImage : ImageView = findViewById(R.id.guitar_image)
+        val guitarSound : MediaPlayer = MediaPlayer.create(this,R.raw.kakon)
+
+        guitarImage.setOnTouchListener{ view, event ->
+
+           if (event.action == MotionEvent.ACTION_DOWN) {
+               guitarImage.setImageResource(R.drawable.comment)
+               guitarSound.seekTo(0)
+               guitarSound.start()
+           }
+
+            else if (event.action == MotionEvent.ACTION_UP){
+                guitarImage.setImageResource(R.drawable.bag)
+            }
+            true
+        }
+
     }
 }
